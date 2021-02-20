7030e6ed1fc3fcb717b975cd219c25d1df9d4efd
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2021-02-20 13:30:27 -0500

    Use simpler algorithm to calculate circle

--- main.py
+++ main.py
@@ -23,51 +23,13 @@ def findCircle(tri):
     x1, y1 = points[tri[0]]
     x2, y2 = points[tri[1]]
     x3, y3 = points[tri[2]]
-
-    x12 = x1 - x2;
-    x13 = x1 - x3;
-
-    y12 = y1 - y2;
-    y13 = y1 - y3;
-
-    y31 = y3 - y1;
-    y21 = y2 - y1;
-
-    x31 = x3 - x1;
-    x21 = x2 - x1;
-
-    # x1^2 - x3^2
-    sx13 = pow(x1, 2) - pow(x3, 2);
-
-    # y1^2 - y3^2
-    sy13 = pow(y1, 2) - pow(y3, 2);
-
-    sx21 = pow(x2, 2) - pow(x1, 2);
-    sy21 = pow(y2, 2) - pow(y1, 2);
-
-    f = (((sx13) * (x12) + (sy13) *
-        (x12) + (sx21) * (x13) +
-        (sy21) * (x13)) // (2 *
-        ((y31) * (x12) - (y21) * (x13))));
-
-    g = (((sx13) * (y12) + (sy13) * (y12) +
-        (sx21) * (y13) + (sy21) * (y13)) //
-        (2 * ((x31) * (y12) - (x21) * (y13))));
-
-    c = (-pow(x1, 2) - pow(y1, 2) -
-        2 * g * x1 - 2 * f * y1);
-
-    # eqn of circle be x^2 + y^2 + 2*g*x + 2*f*y + c = 0
-    # where centre is (h = -g, k = -f) and
-    # radius r as r^2 = h^2 + k^2 - c
-    h = -g;
-    k = -f;
-    sqr_of_r = h * h + k * k - c;
-
-    # r is the radius
-    r = round(sqrt(sqr_of_r), 5);
-
-    return ((h,k),r)
+    
+    x, y, z = complex(x1, y1), complex(x2, y2), complex(x3, y3)
+    w = z-x
+    w /= y-x
+    c = (x-y)*(w-abs(w)**2)/2j/w.imag-x
+    
+    return ((-c.real,-c.imag),abs(c+x))
 
 def sharedSegmentPolygon(tri, poly):
     for t in combinations(tri, 2):
